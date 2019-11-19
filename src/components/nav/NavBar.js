737945dcbfe18b6212be405f1d623ba9db5124b9
Schatfield <dgoldmine@icloud.com>
2019-11-19 14:48:37 -0600

    code from class

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -16,9 +16,9 @@ class NavBar extends Component {
           <ul className="container">
             <li><Link className="nav-link" to="/">Home</Link></li>
             <li><Link className="nav-link" to="/animals">Animals</Link></li>
-            <li><Link classname="nav-link" to="/locations">Locations</Link></li>
-            <li><Link classname="nav-link" to="/employees">Employees</Link></li>
-            <li><Link classname="nav-link" to="/owners">Owners</Link></li>
+            <li><Link className="nav-link" to="/locations">Locations</Link></li>
+            <li><Link className="nav-link" to="/employees">Employees</Link></li>
+            <li><Link className="nav-link" to="/owners">Owners</Link></li>
           </ul>
         </nav>
       </header>
