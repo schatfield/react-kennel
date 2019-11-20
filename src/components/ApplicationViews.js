73b53bb73f15f5844e8874ce661c7d43ee932e21
Schatfield <dgoldmine@icloud.com>
2019-11-20 14:03:49 -0600

    added dynamic routing in class for animals

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,6 +6,7 @@ import AnimalList from './animal/AnimalList'
 import LocationCard from './location/LocationCard'
 import EmployeeCard from './employee/EmployeeCard'
 import OwnerCard from './owner/OwnerCard'
+import AnimalDetail from './animal/AnimalDetail'
 
 
 class ApplicationViews extends Component {
@@ -16,9 +17,25 @@ class ApplicationViews extends Component {
         <Route exact path="/" render={(props) => {
           return <Home />
         }} />
-        <Route path="/animals" render={(props) => {
-          return <AnimalList />
-        }} />
+
+        {/* Make sure you add the `exact` attribute here */}
+<Route exact path="/animals" render={(props) => {
+  return <AnimalList />
+}} />
+<Route path="/animals/:animalId(\d+)" render={(props) => {
+  // Pass the animalId to the AnimalDetailComponent
+  return <AnimalDetail animalId={parseInt(props.match.params.animalId)}/>
+}} />
+
+{/*
+  This is a new route to handle a URL with the following pattern:
+  http://localhost:3000/animals/1
+
+  It will not handle the following URL because the `(\d+)`
+  matches only numbers after the final slash in the URL
+  http://localhost:3000/animals/jack
+*/}
+        
         <Route exact path="/location" render={(props) => {
           return <LocationCard />
         }} />
